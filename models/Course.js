1f4d264f473926a4ae9535da8bca3d4a06a012b6
Rehan <rehanarif.mohammad@gmail.com>
2026-04-25 15:43:24 +0530

    Add filename to course pdf schema

--- models/Course.js
+++ models/Course.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: Number, default: 0 }, // 0 = free
+  price: { type: Number, default: 0 },
   isFree: { type: Boolean, default: false },
   thumbnail: { type: String },
   videos: [{ title: String, url: String }],
-  pdfs: [{ title: String, url: String }],
+  pdfs: [{ title: String, url: String, filename: String }],
   instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
 
